84ca2cf7ddb9574c49cc10a71a0624fd8278fd44
matheusvdes007 <mvsousa5@gmail.com>
2021-07-22 00:17:08 -0300

    Updating project

--- teste.asm
+++ teste.asm
@@ -34,6 +34,35 @@ FlagTiro: var #1
 tamanhoPalavra: var #1
 	static tamanhoPalavra + #0, #9
 
+velocInicialPalavra: var #1
+	static velocInicialPalavra + #0, #7
+
+Rand : var #24			; Tabela de nr. Randomicos entre 1 - 25
+	static Rand + #0, #13
+	static Rand + #1, #5
+	static Rand + #2, #8
+	static Rand + #3, #14
+	static Rand + #4, #2
+	static Rand + #5, #4
+	static Rand + #6, #7
+	static Rand + #7, #9
+	static Rand + #8, #24
+	static Rand + #9, #16
+	static Rand + #10, #12
+	static Rand + #11, #17
+	static Rand + #12, #1
+	static Rand + #13, #19
+	static Rand + #14, #22
+	static Rand + #15, #11
+	static Rand + #16, #18
+	static Rand + #17, #21
+	static Rand + #18, #6
+	static Rand + #19, #20
+	static Rand + #20, #23
+	static Rand + #20, #15
+	static Rand + #21, #25
+	static Rand + #22, #3
+	static Rand + #23, #10
 
 main:
 	call ApagaTela
@@ -161,6 +190,47 @@ ImprimeStr:
 	rts ; return
 	
 
+; Descrição: imprime uma mensagem na tela até encontrar o '\0'
+; Parâmetros: r0 -> posição da tela em que o primeiro caractere da mensagem será impresso
+;             r1 -> endereço da mensagem
+;             r2 -> cor da mensagem
+ImprimeStrInteira:	
+	
+	; Protegendo os conteúdos dos registradores:
+	push r0	
+	push r1	
+	push r2
+	push r3
+	push r4
+	push r5
+	push r6
+	
+	; Carregando constantes:
+	loadn r3, #'\0'	  ; terminador de string
+	loadn r5, #' '	  ; 32 (ASCII espaço)
+
+    ; Loop de impressão da string:
+    ImprimeStr_Loop:	
+		loadi r4, r1
+		cmp r4, r3		; if (charAtual == '\0'), vai embora
+		jeq ImprimeStr_Sai
+		add r4, r2, r4	; soma a cor
+		outchar r4, r0	; imprime o caractere na tela
+    ImprimeStr_Sai:
+	
+	; Recuperando os valores anteriores dos registradores:	
+	pop r6
+	pop r5
+	pop r4
+	pop r3
+	pop r2
+	pop r1
+	pop r0
+	
+	rts ; return
+
+
+
 Delay:
 	
 	; Protegendo o conteúdo dos registradores:
@@ -183,14 +253,132 @@ Delay:
 	
 	rts ; return
 
-move_palavra:		; 
+move_palavra:	; função que move a palavra no vídeo
+				; Parâmetros:
+				;	r0 -> linha atual da palavra
+				;	r1 -> endereço inicial da palavra
+	; Proteção de dados dos registradores
+	push r0 	
+	push r2
+	push r4		
+	push r1
 
-	push r0			; contador de palavras
+	loadn r2, #25 		; limite máximo da linha da palavra
+
+	cmp r0, r2			; if(r0 == r2){ O jogo termina }
+	jeq fimDoJogo
+	
+	loadn r1, #tela0Linha0	; endereço inicial de uma linha vazia
+	
+	push r0
+
+	loadn r4, #40
+	mult r0, r0, r4			; r0 *= r4 (posicão inicial para imprimir na tela)
+	loadn r2, #0000			; cor branca para inserção da palavra na tela
+	
+	call ImprimeStrInteira
+	
+	pop r0					; r0 = (r0 + 1) * r4 (próxima posição da palavra)
+	inc r0
+	mult r0, r0, r4
+	
+	pop r1
+	call ImprimeStrInteira
+
+	pop r4
+	pop r2
+	pop r0
+
+	rts		; return
+
+nova_palavra:		; funcão que imprime uma nova palavra
+			 		; Parâmetros:
+					;	r1 -> endereço inicial para a palavra
+	push r0
+	push r1
+	push r2
+
+					; codar a função limpa palavra 
+
+	loadn r0, #0	; posição inicial da palavra
+	loadn r2, #0000 ; cor branca para impressão da palavra
+
+	call ImprimeStrInteira
+
+	pop r2
+	pop r1
+	pop r0
+
+	rts ; return
+
+
+limpa_palavra:		; função que remove a palavra da tela
+					; Parâmetros:
+					;	r0 -> linha da palavra
+	push r0
+	push r1
+	push r2
+
+	loadn r2, #40	; r0 *= r2 (posicao inicial da palavra a ser removida)
+	mult r0, r0, r2
+
+	loadn r2, #0			; cor branca para impressão da mensagem
+	loadn r1, #tela0Linha0	; linha sem conteúdo para limpar a palavra
+
+	call ImprimeStrInteira
+
+	pop r2
+	pop r1
+	pop r0
+
+	rts
 
 
 
 
 
+
+
+
+
+;move_palavra:		; Função de interação das palavras na tela
+	; preservando o conteúdo dos registradores
+;	push r0			
+;	push r1
+;	push r2
+;	push r3
+;	push r4
+;	push r5
+;	push r6
+;
+;	loadn r0, #24					; quantidade total de palavras
+;	loadn r1, #palavra0				; ponteiro para o inicio do conjunto de palavras
+;	loadn r2, tamanhoPalavra		; carrega o tamanho da palavra 
+;	; ------------------------------------------------------------------
+;	loadn r3, tamanhoPalavra		;
+;	loadn r4, #1					;	
+;	add r3, r3, r4					; r3 = tamanhoPalavra + 1 -> pula para a próxima palavra (precisa pular tamPalavra + '\0')
+;	; -----------------------------------------------------------------
+;	loadn r5, #0					; contador de palavras
+;	loadn r6, velocInicialPalavra   ; contador de velocidade da palavra
+;
+;	move_palavra_Loop:
+;		push r2
+;
+;
+;		pop r2
+;		cmp r5, r0
+;		jle move_palavra_Loop
+;
+;	pop r6
+;	pop r5
+;	pop r4
+;	pop r3
+;	pop r2
+;	pop r1
+;	pop r0
+;
+;	rts
 
 
 
@@ -254,36 +442,92 @@ tela1Linha22 : string "                                        "
 tela1Linha23 : string "                                        "
 tela1Linha24 : string "                                        "
 tela1Linha25 : string "                                        "
-tela1Linha26 : string "                                        "
-tela1Linha27 : string "                    ^                   "
-tela1Linha28 : string "                   / \\                  "
-tela1Linha29 : string "                  |^|^|                 "
+tela1Linha26 : string "                     ^                  "
+tela1Linha27 : string "                   /  \\                 "
+tela1Linha28 : string "                  |^  ^|                "
+tela1Linha29 : string "                  |____|                "
 
+; TELA 2 = tela inicial do joguinho
+tela0Linha0  : string "                                        "
+tela0Linha1  : string "                                        "
+tela0Linha2  : string "                                        "
+tela0Linha3  : string "                                        "
+tela0Linha4  : string "    ._____.  __      __  _____ _____    "
+tela0Linha5  : string "    |_   _|  \\ \\  / / || * | |____|     "
+tela0Linha6  : string "      | |     \\ \\/ /  ||---- |____      "
+tela0Linha7  : string "      | |       |   |   ||     |____|   "
+tela0Linha8  : string "      | |       |   |   ||     |____    "
+tela0Linha9  : string "      |_|       |___|   ||     |____|   "
+tela0Linha10 : string "                                        "
+tela0Linha11 : string "                                        "
+tela0Linha12 : string "       _______  _     _   _____         "
+tela0Linha13 : string "       |_   _| | |   | |  |____|        "
+tela0Linha14 : string "         | |   | |___| |  |____         "
+tela0Linha15 : string "         | |   |  ___  |  |____|        "
+tela0Linha16 : string "         | |   | |   | |  |____         "
+tela0Linha17 : string "         |_|   |_|   |_|  |____|        "
+tela0Linha18 : string "                                        "
+tela0Linha19 : string "                                        "
+tela0Linha20 : string "   ___               _     _   ______   "
+tela0Linha21 : string "  //__|     /\\     | \\  / |  |_____|    "
+tela0Linha22 : string " //  __    /  \\    |  \\/  |  |_____     "
+tela0Linha23 : string "||  |__|  / /\\ \\  | |\\_/| | |_____|     "
+tela0Linha24 : string "||   //  /_/  \\_\\ | |   |_|  |_____     "
+tela0Linha25 : string " \\_//  /_/     \\_\\ |_|   |_||_____|     "
+tela0Linha26 : string "                                        "
+tela0Linha27 : string "                                        "
+tela0Linha28 : string "                                        "
+tela0Linha29 : string "                                        "
 
 ; palavras para o usuário digitar
 ; COLOCAR DEPOIS AS PALAVRAS AQUI!!!
-ambiguous
-automatic
-available
-catalogue
-chocolate
-catalogue
-detective
-economics
-effective
-essential
-formation
-intention
-invisible
-modernize
-parameter
-performer
-policeman
-practical
-president
-privilege
-secretary
-situation
-strategic
-treatment
-vegetable 
+palavra1  : string "                ambiguous                 "
+palavra2  : string "                automatic                 "
+palavra3  : string "                available                 "
+palavra4  : string "                catalogue                 "
+palavra5  : string "                chocolate                 "
+palavra6  : string "                catalogue                 "
+palavra7  : string "                detective                 "
+palavra8  : string "                economics                 "
+palavra9  : string "                effective                 "
+palavra10 : string "                essential                 "
+palavra11 : string "                formation                 "
+palavra12 : string "                intention                 "
+palavra13 : string "                invisible                 "
+palavra14 : string "                modernize                 "
+palavra15 : string "                parameter                 "
+palavra16 : string "                performer                 "
+palavra17 : string "                policeman                 "
+palavra18 : string "                practical                 "
+palavra19 : string "                president                 "
+palavra20 : string "                privilege                 "
+palavra21 : string "                secretary                 "
+palavra22 : string "                situation                 "
+palavra23 : string "                strategic                 "
+palavra24 : string "                treatment                 "
+palavra25 : string "                vegetable                 "
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
